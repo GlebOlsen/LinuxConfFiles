e2c43033e0937da7ca2070175f95dc609c8f900f
glebolsen <gleb.olsen@gmail.com>
2024-06-25 16:17:07 +0200

    +LSP +CMP

--- nvim/init.lua
+++ nvim/init.lua
@@ -84,6 +84,54 @@ require('packer').startup(function(use)
     end
   }
 
+  -- Add nvim-lspconfig for LSP support
+--  use {
+--    'neovim/nvim-lspconfig',
+--    config = function()
+--      require('lspconfig').volar.setup {} -- this is example for vue
+--    end
+--  }
+
+  -- Add nvim-cmp for autocompletion
+  use {
+    'hrsh7th/nvim-cmp',
+    requires = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip'
+    },
+    config = function()
+      local cmp = require'cmp'
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        },
+        mapping = {
+          ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+          ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+          ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+          ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+          ['<C-e>'] = cmp.mapping({
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close(),
+          }),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        },
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+        }, {
+          { name = 'buffer' },
+        })
+      })
+    end
+  }
+
   -- Add an autocommand to compile packer whenever the init.lua file is saved
   vim.cmd([[
     augroup packer_user_config
