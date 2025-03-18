719b3d73308733e3b99322b4ddda67f1d1814805
glebolsen <gleb.olsen@gmail.com>
2025-03-18 14:55:28 +0100

    Updated neovim config

--- terminalEditors/nvim/init.lua
+++ terminalEditors/nvim/init.lua
@@ -7,21 +7,20 @@ require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   -- Add lualine.nvim
-  -- On line 17 you can set your own theme
   use {
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons' },
     config = function()
       require('lualine').setup {
         options = {
-          theme = 'powerline',
+          theme = 'powerline', -- Change lualine theme here
           section_separators = {'', ''},
           component_separators = {'|', '|'},
         },
         sections = {
           lualine_a = {'mode'},
           lualine_b = {'branch'},
-          lualine_c = {'filename'},
+          lualine_c = {{'filename', path = 1}}, -- Use fullpath here
           lualine_x = {'encoding', 'fileformat', 'filetype'},
           lualine_y = {'progress'},
           lualine_z = {'location'}
@@ -29,6 +28,7 @@ require('packer').startup(function(use)
       }
     end
   }
+
   -- Add gitsigns.nvim
   use {
     'lewis6991/gitsigns.nvim',
@@ -67,24 +67,48 @@ require('packer').startup(function(use)
         },
         hijack_netrw = true,
         update_cwd = true,
-        filters = {
-          custom = {},  -- Empty custom filters to show all files and directories
-          dotfiles = false,  -- Optionally show dotfiles
-          git_ignored = false,  -- Show files and directories ignored by Git
-        }
       }
     end
   }
-
   -- Add nvim-lspconfig for LSP support
-  use {
-    'neovim/nvim-lspconfig',
-    config = function()
-      require('lspconfig').clangd.setup {} -- C support (Needs clangd)
-      require('lspconfig').pyright.setup {} -- python3 support (Needs npm install -g pyright)
-      -- require('lspconfig').volar.setup {} -- this is example for vue
-    end 
-}
+    use {
+      'neovim/nvim-lspconfig',
+      config = function()
+        local capabilities = require('cmp_nvim_lsp').default_capabilities() -- Get capabilities from cmp-nvim-lsp
+
+        local on_attach = function(client, bufnr)
+            -- Add common keymaps, like gd (go to definition), K (hover), etc.
+             local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+             local opts = { noremap=true, silent=true }
+                buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+                buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+                buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+                buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+                buf_set_keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+                buf_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+                buf_set_keymap('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+                buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+                buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+                buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+                buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+                buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+        end
+	
+	-- C/C++
+        require('lspconfig').clangd.setup {
+          capabilities = capabilities,
+          on_attach = on_attach
+        }
+
+        -- Python
+        require('lspconfig').pyright.setup {
+          capabilities = capabilities,
+          on_attach = on_attach
+        }
+
+        -- Add more language servers here
+      end
+    }
 
   -- Add nvim-cmp for autocompletion
   use {
@@ -105,51 +129,64 @@ require('packer').startup(function(use)
             require('luasnip').lsp_expand(args.body)
           end,
         },
-        mapping = {
-          ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-          ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-          ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-          ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-          ['<C-e>'] = cmp.mapping({
-            i = cmp.mapping.abort(),
-            c = cmp.mapping.close(),
-          }),
+        mapping = cmp.mapping.preset.insert({ -- Use a preset for common mappings
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-n>'] = cmp.mapping.complete(), -- Changed to C-n.  More reliable.
+          ['<C-e>'] = cmp.mapping.abort(),   -- Use abort() directly
           ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        },
-        sources = cmp.config.sources({
+          ['<Tab>'] = cmp.mapping(function(fallback)  -- Example of a more complex mapping
+              if cmp.visible() then
+                  cmp.select_next_item()
+              elseif require('luasnip').expand_or_jumpable() then
+                  vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+              else
+                  fallback()
+              end
+          end, { 'i', 's' }), -- i = insert mode, s = select mode
+          ['<S-Tab>'] = cmp.mapping(function(fallback) -- Shift-tab example
+              if cmp.visible() then
+                  cmp.select_prev_item()
+              elseif require('luasnip').jumpable(-1) then
+                  vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
+              else
+                  fallback()
+              end
+          end, { 'i', 's' }),
+        }),
+        sources = {
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
-        }, {
-          { name = 'buffer' },
-        })
+          { name = 'buffer' },  -- Add buffer completions here
+          { name = 'path' }, -- add path completions
+        }
       })
     end
   }
 
   -- Add an autocommand to compile packer whenever the init.lua file is saved
-  vim.cmd([[
-    augroup packer_user_config
-      autocmd!
-      autocmd BufWritePost init.lua source <afile> | PackerCompile
-    augroup end
-  ]])
+    vim.cmd([[
+      augroup packer_user_config
+        autocmd!
+        autocmd BufWritePost init.lua lua if pcall(vim.cmd, 'source ' .. vim.fn.expand('<afile>')) then require('packer').sync() end
+      augroup end
+    ]])
 end)
 
 -- Set the background to be transparent
-vim.cmd [[
-  highlight Normal guibg=NONE ctermbg=NONE
-  highlight NonText guibg=NONE ctermbg=NONE
-]]
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NonText", { bg = "none" })
+
 
 -- Set key mappings for :Gitsigns
-vim.keymap.set("n", "<leader>gp", function() require('gitsigns').preview_hunk_inline() end, {})
-vim.keymap.set("n", "<leader>gt", function() require('gitsigns').toggle_current_line_blame() end, {})
+vim.keymap.set("n", "<leader>gp", "<cmd>lua require('gitsigns').preview_hunk_inline()<CR>")
+vim.keymap.set("n", "<leader>gt", "<cmd>lua require('gitsigns').toggle_current_line_blame()<CR>")
 
 -- Set key mappings for :Telescope
-vim.api.nvim_set_keymap('n', '<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<cr>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>fg', "<cmd>lua require('telescope.builtin').live_grep()<cr>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>fb', "<cmd>lua require('telescope.builtin').buffers()<cr>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>fh', "<cmd>lua require('telescope.builtin').help_tags()<cr>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<cr>", {})
+vim.api.nvim_set_keymap('n', '<leader>fg', "<cmd>lua require('telescope.builtin').live_grep()<cr>", {})
+vim.api.nvim_set_keymap('n', '<leader>fb', "<cmd>lua require('telescope.builtin').buffers()<cr>", {})
+vim.api.nvim_set_keymap('n', '<leader>fh', "<cmd>lua require('telescope.builtin').help_tags()<cr>", {})
 
 -- Set key mappings for :NvimTree
 vim.api.nvim_set_keymap('n', '<leader>e', ":NvimTreeToggle<CR>", {noremap = true, silent = true})
@@ -163,7 +200,6 @@ vim.opt.wrap = true
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.mouse = 'a'
 vim.opt.termguicolors = true
-vim.opt.statusline = '%F'
+-- vim.opt.statusline = '%F' For normal VIM
 
-vim.cmd [[highlight CursorLine guibg=#49115b]]
-
+vim.api.nvim_set_hl(0, "CursorLine", { bg = "#49115b" })
